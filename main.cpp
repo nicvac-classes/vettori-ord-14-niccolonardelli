334b84c6f69f8fc9096f6b72b8a14375bd80c2c6
niccolonardelli <niccolo.nardelli@marconibari.edu.it>
2025-03-27 09:32:15 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -1,11 +1,54 @@
 #include <iostream>
+#include <vector>
+#include <ctime>
 using namespace std;
-
-int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+int main() 
+{
+    srand(time(0));
+    int i, n, j, t;
+    string s;
+    bool scambio;
+    cout << "quanti studenti ci sono nella classe?" << endl;
+    cin >> n;
+    vector<int>r(n);
+    vector<string>nomi(n);
+    i=0;
+    while (i<n)
+    {
+        cout << "come ti chiami?" << endl;
+        cin >> nomi[i];
+        cout << "qual è il tuo voto?" << endl;
+        cin>>n;
+    }
+    i=0;
+    scambio = true;
+    while (i<n and scambio)
+    {
+        j=0;
+        scambio= false;
+        while (j<=n-2-i)
+        {
+            if (r[j]>r[j+1])
+            {
+                t=r[j];
+                r[j]= r[j+1];
+                r[j+1] = t;
+                s=nomi[j];
+                nomi[j]=nomi[j+1];
+                nomi[j+1] =s;
+                scambio=true;
+            }
+            j=j+1;
+        }
+        i=i+1;
+    }
+    j=0;
+    while(j<n)
+    {
+        cout << "lo studente " << nomi[j] << " ha voto d'informatica " << r[j] << endl;
+        j=j+1;
+    }
 }
 
 //LEGGERE LE ISTRUZIONI NEL FILE README.md
+
